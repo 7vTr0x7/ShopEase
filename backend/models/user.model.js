18853f6b5fb432dc7a233be083c0e2f6db21f2b9
7vTr0x7 <7vTrox7@gmail.com>
2024-09-27 20:31:47 +0530

    user.model

---
+++ backend/models/user.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const ShopEaseUserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  addresses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ShopEaseAddress",
+    },
+  ],
+  cart: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ShopEaseProduct",
+    },
+  ],
+  wishlist: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ShopEaseProduct",
+    },
+  ],
+});
+
+const ShopEaseUser = mongoose.model("ShopEaseUser", ShopEaseUserSchema);
