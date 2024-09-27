58e8a5e595ddd7d6ea4623937e3885eba5759b89
7vTr0x7 <7vTrox7@gmail.com>
2024-09-27 20:40:19 +0530

    server

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 const ShopEaseUserSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: true,
+    required: true,
   },
   email: {
     type: String,
-    require: true,
+    required: true,
     unique: true,
   },
   password: {
     type: String,
-    require: true,
+    required: true,
   },
   addresses: [
     {
