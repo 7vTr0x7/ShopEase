ac1836d8eb96fc9471d26f105d7bdc7ff7b25bb9
7vTr0x7 <7vTrox7@gmail.com>
2024-09-28 18:48:36 +0530

    address.model

---
+++ backend/models/address.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const ShopEaseAddressSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "ShopEaseUser" },
+  houseNo: { type: String, required: true },
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  country: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  mobileNumber: { type: Number, required: true },
+});
+
+const ShopEaseAddress = mongoose.model(
+  "ShopEaseAddress",
+  ShopEaseAddressSchema
+);
+
+module.exports = ShopEaseAddress;
