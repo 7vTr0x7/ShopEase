2cc5da95dbc119a069a35f4c7a17a6a711c8a672
7vTr0x7 <7vTrox7@gmail.com>
2024-09-29 17:40:36 +0530

    feat: isAuthenticated

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,1 +1,18 @@
-export const isAuthenticated = async (req, res, next) => {};
+import ShopEaseUser from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+import { config } from "dotenv";
+
+config({ path: "D:/shopease/backend/.env" });
+
+export const isAuthenticated = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(404).json({ message: "Login First" });
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await ShopEaseUser.findById(decoded._id);
+  next();
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getMyProfile, loginUser, registerUser } from "../controllers/user.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/user", getMyProfile);
+router.get("/user", isAuthenticated, getMyProfile);
 
 //router.route("/user/:userId").get(getUserById).post().delete() // route chaining
 
