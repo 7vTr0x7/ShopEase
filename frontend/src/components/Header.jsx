1d4729ca677691b41426aa3f21daebe558bfff42
7vTr0x7 <7vTrox7@gmail.com>
2024-10-05 18:47:40 +0530

    changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,11 +3,11 @@ import logo from "../images/logo.png";
 
 const Header = () => {
   return (
-    <header className="bg-purple-100 py-3 px-6">
+    <header className="bg-black opacity-90 py-3 px-6">
       <div className="flex justify-between">
         <div className="flex items-center">
           <img alt="logo" src={logo} className="h-14" />
-          <p className="text-purple-400 text-xl font-bold cursor-pointer hover:text-purple-500">
+          <p className="text-white text-xl font-bold cursor-pointer hover:text-purple-500">
             ShopEase
           </p>
         </div>
@@ -18,20 +18,12 @@ const Header = () => {
             className="py-1 px-3 focus:outline-none rounded-lg  w-3/12"
           />
         </div>
-        <div className="flex  gap-10 items-center  text-lg text-purple-500">
-          <p className="  font-bold cursor-pointer hover:text-purple-400">
-            Explore
-          </p>
+        <div className="flex  gap-10 items-center  text-lg text-white">
+          <p className="  font-bold cursor-pointer hover:text-gray-400">Explore</p>
 
-          <p className=" font-bold cursor-pointer hover:text-purple-400">
-            Wishlist
-          </p>
-          <p className=" font-bold cursor-pointer hover:text-purple-400">
-            Cart
-          </p>
-          <p className=" font-bold cursor-pointer hover:text-purple-400">
-            Login
-          </p>
+          <p className=" font-bold cursor-pointer hover:text-gray-400">Wishlist</p>
+          <p className=" font-bold cursor-pointer hover:text-gray-400">Cart</p>
+          <p className=" font-bold cursor-pointer hover:text-gray-400">Login</p>
         </div>
       </div>
     </header>
--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -2,21 +2,21 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="flex items-center relative  mx-8 my-6  bg-purple-200 rounded-lg">
+    <div className="flex items-center relative  mx-8 my-6  bg-black rounded-lg">
       <div>
         <img
           alt="product"
-          className="rounded-lg opacity-10"
+          className="rounded-lg opacity-15"
           src={
             "https://images.macrumors.com/article-new/2024/09/iphone-16-design.jpg"
           }
         />
       </div>
       <div className="absolute px-10 flex justify-between ">
-        <div className="text-purple-500">
+        <div className="text-white">
           <p className="text-7xl font-bold">Explore The </p>
           <p className="text-7xl font-bold">Ultimate Collection</p>
-          <button className="mt-8 text-2xl font-bold border px-3 py-1 border-purple-400 rounded-lg hover:text-white hover:bg-purple-500">
+          <button className="mt-8 text-2xl font-bold border px-3 py-1 border-white rounded-lg hover:text-black hover:bg-white">
             Shop Now
           </button>
         </div>
