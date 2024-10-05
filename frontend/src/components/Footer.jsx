2deb36208d06a535f1e19e98cf13de5eccbb66b6
7vTr0x7 <7vTrox7@gmail.com>
2024-10-05 18:48:46 +0530

    changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-purple-100 text-purple-600 text-xl font-semibold text-center py-6">
+    <footer className="bg-black text-white text-xl font-semibold text-center py-6">
       <p>&copy; 2024 ShopEase. All rights reserved.</p>
     </footer>
   );
