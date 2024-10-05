deb242c2b2450b96ef0567b99dff209ed0c8b381
7vTr0x7 <7vTrox7@gmail.com>
2024-10-05 17:42:09 +0530

    Hero

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="flex relative justify-center mx-8 my-6  bg-gray-200 rounded-lg">
+    <div className="flex items-center relative  mx-8 my-6  bg-gray-200 rounded-lg">
       <div>
         <img
           alt="product"
@@ -12,14 +12,14 @@ const HeroSection = () => {
           }
         />
       </div>
-      <div className="absolute right-0 bottom-0 ">
-        {/* <img
-          alt="product"
-          className="h-20"
-          src={
-            "https://images.macrumors.com/article-new/2024/09/iphone-16-design.jpg"
-          } */}
-        {/* /> */}
+      <div className="absolute px-10 flex justify-between ">
+        <div className="text-purple-500">
+          <p className="text-7xl font-bold">Explore The </p>
+          <p className="text-7xl font-bold">Ultimate Collection</p>
+          <button className="mt-8 text-2xl font-bold border px-3 py-1 border-purple-400 rounded-lg hover:text-white hover:bg-purple-500">
+            Shop Now
+          </button>
+        </div>
       </div>
     </div>
   );
