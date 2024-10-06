fd834bf6736b7c03f74f2964a700f5efcc668876
7vTr0x7 <7vTrox7@gmail.com>
2024-10-07 01:58:09 +0530

    cart

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { FaRegHeart } from "react-icons/fa";
 import { MdOutlineStar } from "react-icons/md";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ prod }) => {
   const navigate = useNavigate();
+  const user = useSelector((state) => state.user.user);
+
+  const addToCartHandler = async () => {};
 
   return (
     <div className="flex items-center bg-white flex-col rounded-lg justify-between p-4 shadow-lg transition-all duration-200 hover:shadow-xl">
@@ -35,9 +39,19 @@ const ProductCard = ({ prod }) => {
         </p>
       </div>
       <div className="mt-4 grid grid-cols-4 gap-2 w-full">
-        <button className="col-span-3 px-4 py-2 text-white bg-black hover:text-black hover:bg-gray-200 rounded-lg font-semibold transition-all duration-200">
-          Add To Cart
-        </button>
+        {user.cart.includes(prod._id) ? (
+          <button
+            onClick={() => navigate("/cart")}
+            className="col-span-3 px-4 py-2 text-white bg-black hover:text-black hover:bg-gray-200 rounded-lg font-semibold transition-all duration-200">
+            Go To Cart
+          </button>
+        ) : (
+          <button
+            onClick={addToCartHandler}
+            className="col-span-3 px-4 py-2 text-white bg-black hover:text-black hover:bg-gray-200 rounded-lg font-semibold transition-all duration-200">
+            Add To Cart
+          </button>
+        )}
         <span className="col-span-1 flex justify-center items-center bg-gray-200 hover:bg-gray-300 rounded-lg transition-all duration-200 cursor-pointer">
           <FaRegHeart className="text-black text-2xl" />
         </span>
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -4,26 +4,6 @@ import CartItem from "../components/CartItem";
 import CheckoutSummary from "../components/CheckoutSummary";
 import Header from "../components/Header";
 
-// Sample product data
-const products = [
-  {
-    _id: "1",
-    name: "Apple iPhone 14 Pro Max",
-    imageUrl: "https://picsum.photos/id/1012/200/200",
-    price: 1199,
-    discountPercent: 10,
-    rating: 4.8,
-    reviews: 400,
-    categories: {
-      category: "Electronics",
-      subCategory: "Smartphones",
-    },
-    availability: true,
-    description:
-      "The latest iPhone 14 Pro Max with advanced features, including A16 Bionic chip, 48MP camera system, and ProMotion display.",
-  },
-];
-
 const calculateDiscountPrice = (price, discountPercent) => {
   return price - (price * discountPercent) / 100;
 };
