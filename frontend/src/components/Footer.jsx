34a3ea5752db1770912c7bc348d3e6afa313ee6c
7vTr0x7 <7vTrox7@gmail.com>
2024-10-07 01:41:56 +0530

    Profile

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="  bg-gray-100 text-black text-xl font-semibold text-center py-6">
       <p>&copy; 2024 ShopEase. All rights reserved.</p>
       <div className="text-3xl mt-3 gap-8 flex justify-center">
-        <FaGithub />
-        <FaLinkedin />
+        <Link to="https://github.com/7vTr0x7">
+          <FaGithub />
+        </Link>
+        <Link to="https://www.linkedin.com/in/salmanshaikh-/">
+          <FaLinkedin />
+        </Link>
       </div>
     </footer>
   );
